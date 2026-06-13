527f9bb2c05b288a988991a7215c50c41368a480
BimboBabatola <bimbo@woodcoreapp.com>
2026-06-13 22:20:04 +0100

    added artiste metadata

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -4,10 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class EnvConstants:
-    FTP_HOST = '195.35.49.228'
-    FTP_USER =  'u251061335.turntable'
-    FTP_PASS = 'Turntable2020.'
-    GPG_PASSPHRASE = 'q#HMPrDK&h4J9@J%dHbH3'
     DB_HOST=os.getenv('DB_HOST')
     DB_USER=os.getenv('DB_USER')
     DB_PASSWORD=os.getenv('DB_PASSWORD')
